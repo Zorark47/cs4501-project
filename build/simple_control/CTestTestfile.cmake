ecfbec92fcd0c017e9e3a8ede7bdaadd55f506d4
Zorark47 <79418288+Zorark47@users.noreply.github.com>
2022-11-18 16:56:46 -0500

    Delete build directory

--- build/simple_control/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/CS4501-Labs/project/src/simple_control
-# Build directory: /root/CS4501-Labs/project/build/simple_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
