ac41e3a0eabcf4e0c1e01af699ed4a018c00a951
Zach <zjw5qs@virginia.edu>
2022-11-18 20:59:50 +0000

    made global planne

---
+++ src/simple_control/src/global_planner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+import tf2_ros
+import time
+import copy
+import math
+from geometry_msgs.msg import Vector3
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from geometry_msgs.msg import PointStamped, PoseStamped, TransformStamped, Quaternion
+from tf2_geometry_msgs import do_transform_point
+
+class GlobalPlanner:
+
+    def __init__(self):
+        self.dog_pos_sub = rospy.Subcriber("/cell_tower/position", PoseStamped, self.get_pos, queue_size=1)
+        self.goal = 0
+
+    def get_pos(self, msg):
+        self.goal = msg
